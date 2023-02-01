e3e18cb47e1d32f05bdb99d068eb44fe2a5b3f61
Hiro6634 <hiro.suyama@gmail.com>
2023-01-31 22:17:54 -0300

    Get con paramtero

--- src/tuits/tuits.controller.ts
+++ src/tuits/tuits.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('tuits')
 export class TuitsController {
     @Get()
     getTuits(){
         return 'Hello World from Tuitter';
+    }
+
+    @Get(':id')
+    getTuit(@Param() params){
+        return `Your tuit id is ${params.id}`;
     }
 }
  
