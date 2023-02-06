f09852d69d08d99ef865fc348c0c1d262ac30d6d
Hiro6634 <hiro.suyama@gmail.com>
2023-02-06 13:20:32 -0300

    Acciones de mantenimiento

--- src/tuits/tuits.controller.ts
+++ src/tuits/tuits.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Param, Post, Patch, Delete, Body, HttpCode, HttpStatus } from '@nestjs/common';
 
 @Controller('tuits')
 export class TuitsController {
@@ -14,8 +14,18 @@ export class TuitsController {
 
     @Post()
     @HttpCode(HttpStatus.NO_CONTENT)
-    createTuit(@Body('message') message: string){
+    createTuit(@Body('message') message: string): string{
       return `Yout tuit was: ${message}`;
+    }  
+
+    @Patch(':id')
+    updateTuit(@Param('id') id: string): string{
+      return `The tuit ${id} was updated`;
+    }  
+
+    @Delete(':id')
+    deleteTuit(@Param('id') id: string, @Body() tuit): string{
+        return `The tuit ${id} was deleted`;
     }  
 }
  
